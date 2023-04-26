d5fffa266225fa4d12da0b92bfd8a1e536ee384d
Aaditya Neupane <aaditya9803@gmail.com>
2023-04-26 21:40:09 +0545

    subnav

---
+++ src/components/Hero.vue
@@ -0,0 +1,9 @@
+<template>
+  <main><section>Hello</section></main>
+</template>
+
+<script>
+export default {
+  name: "Hero_1",
+};
+</script>
--- src/components/Subnav.vue
+++ src/components/Subnav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-16 bg-white border-b border-solid border-brand-blue-1">
     <div class="flex items-center h-full px-8">
-      <div>
+      <div v-if="onJobsResultsPage" data-test="job-count">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
         <span>
           <span class="text-brand-green-1">123</span>
@@ -14,5 +14,10 @@
 <script>
 export default {
   name: "Subnav_1",
+  data() {
+    return {
+      onJobResultsPage: true,
+    };
+  },
 };
 </script>
